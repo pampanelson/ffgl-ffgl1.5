97c0b711230e21285dc2f4e3d2cf558666f75ba5
Pampa Nelson <pampa.nelson@gmail.com>
2019-08-20 16:45:50 +0800

    50 random wave may works at 30 fps 4320x1080p

--- source/plugins/AddSubtract/Frag0.h
+++ source/plugins/AddSubtract/Frag0.h
@@ -20,9 +20,10 @@ uniform float switchTex;
 uniform float float1;
 
 const float PI = 3.1415926535;
-float lineNumF = 80.0;
+//float lineNumF = 80.0;
 float lineWidith = 0.003;
 float offsetY = 0.2;
+
 
 
 // Polynomial smooth min (for copying and pasting into your shaders)
@@ -35,21 +36,73 @@ float smax(float a,float b,float k){
     return smin(a,b,-k);
 }
 
-float wave1(float x){
-    return x;
+float hash1(float s){
+    return sin(s * 42.6520981210954362);
 }
-float wave2(float x,float amplitude,float freq){
-    float res = 0.0;
+
+float hash2(float s){
+    return fract(s * 24.57608761367653 + 0.4146145118678);
+}
+
+float wave1(float stx,float scale,float t){    // float angle = abs(sin(iTime))* 1.5;
     
-    res = amplitude - pow(x*freq,2.0);
+    
+    //float angle = .3 + 0.4 * abs(sin(iTime)) - 0.12;// 0.3~0.5
+    //float angle = .3 + 0.4 * a - 0.12;// 0.3~0.5
+    
+    // stx += angle;
+    float hash1 = hash1(t);
+    // float hash2 = hash2(iTime);
+    float r;
+    float r1 = (0.1 + 0.0001 * hash1) * abs(sin((stx + 0.1*t) * 4.1));
+    float r2 = (0.12 - 0.001 * hash1) * sin(stx*14. + t);
+    float r3 = (.35 + 0.0001 * hash1) * cos(stx*18. + t)-0.6;
+    
+    
+    r = smax(r,r1,0.6);
+    r = smax(r,r2,.2);
+    r = smax(r,r3,.6);
+    r *= scale;
+    r = max(0.0,r);
+    return r;
+}
+
+float wave2(float stx,float scale){
+    float res;
+    
+    stx = 4.*(stx-0.5);// input stx is 0.25~0.75 after uv.y += 0.5
+    // now convert stx to -1 ~ 1 for local point up curve
+    res = 1. - abs(stx) - 0.1; // move sharp peak lower bit
+    float res1 = 1. - pow(stx,2.0);
+    res = smax(res,res1,0.2); // linear make very sharp peak,mix with powered smooth peak
+    res *= scale; // scale down
+    res = max(0.0,res);
+    
     return res;
 }
-// peak is highest point , narrow is wave wide
-float wave3(float x,float peak,float narrow){
-    float res = 0.0;
-    res = peak - narrow*abs(x*x*x);
-    return res;
+
+float wave(float stx,float t,float scale){
+    // st.x += 0.25 * sin(iTime);
+    // a = abs(sin(iTime));
+    float w1 = wave1(stx,0.7,t);
+    float w2 = wave2(stx,0.15);
+    
+    // for debug
+    // if(st.y < w1){
+    //     col += vec3(1.0,0.0,0.0);
+    // }
+    
+    // if(st.y < w2){
+    //     col += vec3(0.0,1.0,0.0);
+    // }
+    
+    float w = smax(w1,w2,0.02);
+    w *= scale;
+    return w;
 }
+
+                                                        
+                                                        
 
 void main()
 {
@@ -64,56 +117,34 @@ void main()
     
     vec3 col;
     
-    vec2 uv = (fragCoord.xy - .5 * iResolution.xy)/iResolution.x; // uv -.5 ~ .5  , x axis is scale t0 1.
-//    uv *= 2.0; // -1. ~ 1.
-//    uv.y += iResolution.y/iResolution.x;// origin point on (0.5 * x , 0.0)
-//    // uv *= 0.5;// 0 ~ 1
-//    vec2 st = vec2(atan(uv.x,uv.y),length(uv));
-//    st.x += PI;// 0 ~ 2PI on -y axis
-//    int tryLoop = 6;
-//    for(int i = 0;i < tryLoop;i++ ){
-//        float factor = 0.04 + 0.01 * sin(iTime*4.0);
-//        st.y += factor * sin(0.0007*iTime*(st.x+iTime*0.1));
-//        
-//        float index = floor(st.y  * lineNumF);
-//        
-//        float gap = 1.0 / lineNumF;
-//        float r = index * gap;
-//        float line = 1.0 - smoothstep(0.0,lineWidith,abs(st.y - r));
-//        
-//        col += line;
-//    }
-//
-//    
-//    if(st.y < .15){
-//        col += 1.0;
-//    }
-//
-//    
+    vec2 uv = (fragCoord - .5 * iResolution.xy)/iResolution.y; // uv -.5 ~ .5
+    uv.y += 0.5;
+    //vec2 uv = fragCoord.xy/iResolution.xy;
+    vec2 st = vec2(atan(uv.x,uv.y),length(uv));
+    st.x = st.x/(PI*2.0) + .5; // before st.x is -π ~ π after is  normalized 0.0 ~ 1.0
+    // origin on -y axis
+    
     
 
-//    vec3 col;
-//
-//    vec2 uv = (fragCoord.xy - .5 * iResolution.xy)/iResolution.x; // uv -.5 ~ .5  , x axis is scale t0 1.
-//    uv *= 2.0; // -1. ~ 1.
-//    uv.y += iResolution.y/iResolution.x;// origin point on (0.5 * x , 0.0)
-//    // uv *= 0.5;// 0 ~ 1
-//    vec2 st = vec2(atan(uv.x,uv.y),length(uv));
-//    st.x += PI;// 0 ~ 2PI on -y axis
-//
-//    vec2 st1 = st;
-//
-//    float index = floor(st.y  * lineNumF);
-//    st1.y += .02 * sin(0.01*iTime*st.x*16.);
-//    float gap = 1.0 / lineNumF;
-//    float r = index * gap;
-//    float line = 1.0 - smoothstep(0.0,lineWidith,abs(st1.y - r));
-//
-//    col += line;
-//
 
 
 //    col = vec3(1.0);
+    float lineNumF = 50.;
+    for(float i = 0.;i<lineNumF;i++){
+        float scale = 0.5;
+        float w = wave1(st.x,scale*i*0.03,iTime*2.0);
+        
+       // if(st.y < w){
+        //    col += 1.0;
+       // }
+        
+        float line = 1.0 - smoothstep(0.0,lineWidith,abs(st.y - w));
+        
+        col += line;
+    }
+
+    
+    
     // Output to screen
     fragColor = vec4(col,1.0);
     
@@ -136,6 +167,69 @@ void main()
 //
 
 // ======================  backup
+
+
+
+
+
+
+
+
+
+
+//
+//vec2 uv = (fragCoord.xy - .5 * iResolution.xy)/iResolution.x; // uv -.5 ~ .5  , x axis is scale t0 1.
+//uv *= 2.0; // -1. ~ 1.
+//uv.y += iResolution.y/iResolution.x;// origin point on (0.5 * x , 0.0)
+//// uv *= 0.5;// 0 ~ 1
+//vec2 st = vec2(atan(uv.x,uv.y),length(uv));
+//st.x += PI;// 0 ~ 2PI on -y axis
+//int tryLoop = 6;
+//for(int i = 0;i < tryLoop;i++ ){
+//    float factor = 0.04 + 0.01 * sin(iTime*4.0);
+//    st.y += factor * sin(0.0007*iTime*(st.x+iTime*0.1));
+//
+//    float index = floor(st.y  * lineNumF);
+//
+//    float gap = 1.0 / lineNumF;
+//    float r = index * gap;
+//    float line = 1.0 - smoothstep(0.0,lineWidith,abs(st.y - r));
+//
+//    col += line;
+//}
+//
+//
+//if(st.y < .15){
+//    col += 1.0;
+//}
+//
+//
+//
+//
+//vec3 col;
+//
+//vec2 uv = (fragCoord.xy - .5 * iResolution.xy)/iResolution.x; // uv -.5 ~ .5  , x axis is scale t0 1.
+//uv *= 2.0; // -1. ~ 1.
+//uv.y += iResolution.y/iResolution.x;// origin point on (0.5 * x , 0.0)
+//// uv *= 0.5;// 0 ~ 1
+//vec2 st = vec2(atan(uv.x,uv.y),length(uv));
+//st.x += PI;// 0 ~ 2PI on -y axis
+//
+//vec2 st1 = st;
+//
+//float index = floor(st.y  * lineNumF);
+//st1.y += .02 * sin(0.01*iTime*st.x*16.);
+//float gap = 1.0 / lineNumF;
+//float r = index * gap;
+//float line = 1.0 - smoothstep(0.0,lineWidith,abs(st1.y - r));
+//
+//col += line;
+
+
+
+
+
+
 
 
 ////    vec2 uv = fragCoord.xy / iResolution.xy;
